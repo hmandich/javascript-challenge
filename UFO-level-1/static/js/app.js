f9c1c27a2bb9cbb13ad2abf02861c1857620de43
storm.breaker <67917417+hillarymandich@users.noreply.github.com>
2021-01-14 19:08:52 -0500

    populatetable

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log("Hello");
+  
+//  create the variables
+var tbody = d3.select("tbody")
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+//Populate data.js date into HTML
+var populatedata = (dataInput) => {
+  dataInput.forEach(ufo_sightings => {
+    var row = tbody.append("tr");
+    columns.forEach(column => row.append("td").text(ufo_sightings[column])
+    )
+  });
+}
+populatedata(data);
+
+
