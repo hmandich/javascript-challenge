7f6749d01d00149ffe7deb36e320225a84c6132a
storm.breaker <67917417+hillarymandich@users.noreply.github.com>
2021-01-16 09:57:48 -0500

    Updates

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -33,32 +33,32 @@ button.on("click", () => {
     
     var inputDate = inputFieldDate.property("value").trim();
     var inputcity = inputFieldCity.property("value").toLowerCase().trim();
-    //var inputstate = inputFieldstate.property("value").toLowerCase().trim();
-    //var inputcountry = inputFieldcountry.property("value").toLowerCase().trim();
-   // var inputshape = inputFieldshape.property("value").toLowerCase().trim();
+    var inputstate = inputFieldstate.property("value").toLowerCase().trim();
+    var inputcountry = inputFieldcountry.property("value").toLowerCase().trim();
+    var inputshape = inputFieldshape.property("value").toLowerCase().trim();
 
     var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
     var filtercity = tableData.filter(tableData => tableData.city === inputcity);
-    var filterData = data.filter(data => data.datetime === inputDate && data.city === inputcity)
-    //var filterstate = tableData.filter(tableData => tableData.datetime === inputDate);
-    //var filtercountry = tableData.filter(tableData => tableData.datetime === inputDate);
-    //var filtershape = tableData.filter(tableData => tableData.datetime === inputDate);
+    var filterstate = tableData.filter(tableData => tableData.state === inputstate);
+    var filtercountry = tableData.filter(tableData => tableData.country === inputcountry);
+    var filtershape = tableData.filter(tableData => tableData.shape === inputshape);
+    var filterData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputcity && tableData.state === inputsate && tableData.county === inputcountry && tableData.shape === inputshape)
 
 // add date filter to table
     tbody.html("");
 
     let response = {
-        filterData, filterDate, filtercity
+        filterData, filterDate, filtercity, filterstate, filtercountry, filtershape
     }
 
     if(response.filterData.length !== 0) {
         populatedata(filterData);
     }
 
-    else if (response.filterData.length === 0 && ((response.filtercity.length !== 0 || response.filterDate.length !== 0))){
-        populatedata(filtercity) || populatedata(filterDate);
+    else if (response.filterData.length === 0 && ((response.filterDate.length !== 0 || response.filtercity.length !== 0 || response.filterstate.length !== 0 || response.filtercountry.length !== 0 || response.filtershape.length !== 0))){
+        populatedata(filterDate) || populatedata(filtercity) || populatedata(filterstate) || populatedata(filtercountry) || populatedata(filtershape);
     }
-    // Top if only works for filtering the date
+    // Top if only works for filtering the dates
     
         else {
             tbody.append("tr").append("td").text("No Sightings on date selected - try again");
@@ -66,6 +66,7 @@ button.on("click", () => {
 })
 //create reset button
 resetbtn.on("click", () => {
+    d3.event.preventDefault();
     tbody.html("");
     populatedata(data)
   })
